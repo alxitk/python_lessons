3bbc38af610847c0e736f6b6d55a8ea5e6332975
alxitk <leshadrenev@gmail.com>
2024-12-24 17:30:36 +0200

    ДЗ 2.2.Виправлене

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# ДЗ 2.2. Необхідно "перевернути" 5-ти значне число
+# # ДЗ 2.2. Необхідно "перевернути" 5-ти значне число
 
+reverse_user_input_number = 0
 user_input_number = int(input("Enter a five-digit number: "))
-print(user_input_number % 10)
+
+reverse_user_input_number = user_input_number % 10 * 10000
 user_input_number = user_input_number//10
-print(user_input_number % 10)
+reverse_user_input_number += (user_input_number % 10) * 1000
 user_input_number //= 10
-print(user_input_number % 10)
+reverse_user_input_number += (user_input_number % 10) * 100
 user_input_number //= 10
-print(user_input_number % 10)
+reverse_user_input_number += (user_input_number % 10) * 10
 user_input_number //= 10
-print(user_input_number % 10)
+reverse_user_input_number += (user_input_number % 10) * 1
+
+print(reverse_user_input_number)
