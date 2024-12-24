efbf103d769138915fde30e0a87fc0a3d08ec410
alxitk <leshadrenev@gmail.com>
2024-12-24 09:41:43 +0200

    ДЗ 2.2. Необхідно перевернути 5-ти значне число

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# print("Hello World")
+# ДЗ 2.2. Необхідно "перевернути" 5-ти значне число
 
-
-# # ДЗ 2.1. Виведення числа в стовпчик
-
-user_input_number = int(input("Enter a four-digit number: "))
-print(user_input_number//1000)
-user_input_number = user_input_number % 1000
-print(user_input_number//100)
-user_input_number = user_input_number % 100
-print(user_input_number//10)
+user_input_number = int(input("Enter a five-digit number: "))
+print(user_input_number % 10)
+user_input_number = user_input_number//10
+print(user_input_number % 10)
+user_input_number //= 10
+print(user_input_number % 10)
+user_input_number //= 10
+print(user_input_number % 10)
+user_input_number //= 10
 print(user_input_number % 10)
