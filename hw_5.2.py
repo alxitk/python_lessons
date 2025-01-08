9a91b5c809fc00d0017bc50d010e6cfcda778e9b
alxitk <leshadrenev@gmail.com>
2025-01-08 09:32:37 +0200

    HW 5.2. Модифікувати калькулятор

---
+++ hw_5.2.py
@@ -0,0 +1,24 @@
+user_input = 'y'
+while user_input == 'y':
+    number_1 = int(input("Enter a number: "))
+    number_2 = int(input("Enter a second number: "))
+    operator = input("Enter the arithmetic operator: ")
+    if operator == '*':
+        result = number_1 * number_2
+    elif operator == '/':
+        if number_2 == 0:
+            result = "Division by zero"     # Исправить при выводе результата
+        else:
+            result = number_1 / number_2
+    elif operator == '+':
+        result = number_1 + number_2
+    elif operator == '-':
+        result = number_1 - number_2
+    print(result if number_2 == 0 else f"{number_1} {operator} {number_2} = {result}")
+    user_input = input('Start calculate again?: y/n ').lower()
+
+
+
+
+
+
